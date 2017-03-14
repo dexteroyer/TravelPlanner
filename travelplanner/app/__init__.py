b4773bbf57b6564a26b75704e8b1ed77f81bcf2d
Chezl <ezlvansa_myxc002@yahoo.com>
2017-03-14 09:11:54 +0800

    Setup project server

--- travelplanner/app/__init__.py
+++ travelplanner/app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.debug = True
 
-
-from auth.views import auth_blueprint
-
-app.register_blueprint(auth_blueprint)
+from app import views
