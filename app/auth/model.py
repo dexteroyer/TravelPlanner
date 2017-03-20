0ba2249163cad0b8969854503abc6e0a6db1a7ba
Shannnnn <sacanoy16@gmail.com>
2017-03-20 10:15:01 +0800

    Slight changes to connect to dashboard

--- app/auth/model.py
+++ app/auth/model.py
@@ -1,8 +1,9 @@
 from app import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
-class User(db.Model):
-    __tablename__ = "users"
+class User(UserMixin, db.Model):
+    tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), nullable=False)
